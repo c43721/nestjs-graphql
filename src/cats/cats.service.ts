cd1202449be6fb1314c3c503c2d72325775b7129
c43721 <rlahman.game@gmail.com>
2021-02-21 19:00:43 -0600

    Redo cat names and add another owner

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,22 +3,29 @@ import { Cat } from './cats.model';
 
 @Injectable()
 export class CatsService {
-    private cats: Cat[] = [{
-        id: 1,
-        name: "1",
-        weight: 10
+  private cats: Cat[] = [
+    {
+      id: 1,
+      name: 'Cat 1',
+      weight: 10,
     },
     {
-        id: 2,
-        name: "2",
-        weight: 20
-    }]
+      id: 2,
+      name: 'Cat 2',
+      weight: 11,
+    },
+    {
+      id: 3,
+      name: 'Cat 3',
+      weight: 8,
+    },
+  ];
 
-    getCatById(id: number): Cat {
-        return this.cats.filter(o => o.id === id)[0];
-    }
+  getCatById(id: number): Cat {
+    return this.cats.filter((o) => o.id === id)[0];
+  }
 
-    getAllCats(): Cat[] {
-        return this.cats;
-    }
+  getAllCats(): Cat[] {
+    return this.cats;
+  }
 }
--- src/owners/owners.service.ts
+++ src/owners/owners.service.ts
@@ -3,17 +3,24 @@ import { Owner } from './owner.model';
 
 @Injectable()
 export class OwnersService {
-    private owners: Owner[] = [{
-        id: 1,
-        name: "Owner 1",
-        cats: [1]
-    }]
+  private owners: Owner[] = [
+    {
+      id: 1,
+      name: 'Owner 1',
+      cats: [1],
+    },
+    {
+      id: 2,
+      name: 'Owner 2',
+      cats: [2, 3],
+    },
+  ];
 
-    getOwnerById(id: number): Owner {
-        return this.owners.filter(o => o.id === id)[0];
-    }
+  getOwnerById(id: number): Owner {
+    return this.owners.filter((o) => o.id === id)[0];
+  }
 
-    getAllOwners(): Owner[] {
-        return this.owners;
-    }
+  getAllOwners(): Owner[] {
+    return this.owners;
+  }
 }
